de48b03ffe145042e577b78f02c0db078955e71d
Fazil Sheik <fazilsheik@aospa.co>
2025-09-23 21:31:28 +0530

    sm8650-common: Switch to QTI usb component
    Change-Id: I39e439f79715d04b71b7c60b759fc643f3eb78cf
    Signed-off-by: anky894 <anky894@proton.me>

--- common.mk
+++ common.mk
@@ -266,7 +266,6 @@ $(call inherit-product, hardware/oplus/overlay/overlay.mk)
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResTargetCommon \
-    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
@@ -301,6 +300,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     overlay \
     perf \
     telephony \
+    usb \
     wfd
 
 # QSPA
@@ -385,18 +385,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
     oplus_usb_compositions.conf
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Verified Boot
 PRODUCT_COPY_FILES += \
