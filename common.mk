0aff44cc7cd06e3267237964837631a8f8f3f951
anky894 <anky894@proton.me>
2026-06-18 17:15:59 +0000

    sm8650-common: Add init.aospa.rc
    Few changes imported from qcom/common
    
    Change-Id: Idf9f787972842ea9e19eaf4ad765e3fdec0e42c7

--- common.mk
+++ common.mk
@@ -160,6 +160,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
+    init.aospa.rc \
     init.class_main.sh \
     init.kernel.post_boot.sh \
     init.kernel.post_boot-cliffs_default_3_4_1.sh \
