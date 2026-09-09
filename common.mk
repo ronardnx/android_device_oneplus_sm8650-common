c05ba7b46a4a88ab87c35915ceb7d6ca95829263
ronardnx <23416091+ronardnx@users.noreply.github.com>
2026-09-09 20:37:07 +0000

    sm8650-common: Include soong namespaces for NXP keymint3 and weaver HALs

--- common.mk
+++ common.mk
@@ -243,6 +243,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+ifneq ($(TARGET_IS_TABLET),true)
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/nxp/keymint/generic \
+    hardware/nxp/weaver/generic
+endif
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
