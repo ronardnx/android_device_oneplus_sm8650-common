2789e2a57025bb972b569f07657c82af20853008
ronardnx <dragosh.rotaru.dragosh@gmail.com>
2026-09-22 05:28:03 +0300

    Revert "sm8650-common: Add a config property for the SMMU proxy driver"
    This reverts commit aca18b5218a25eaab70da813da58fc6296d8feb8.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,9 +212,6 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # SEPolicy
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
-# SMMU
-TARGET_USES_SMMU_PROXY := true
-
 # UFS
 SOONG_CONFIG_NAMESPACES += ufsbsg
 SOONG_CONFIG_ufsbsg += ufsframework
